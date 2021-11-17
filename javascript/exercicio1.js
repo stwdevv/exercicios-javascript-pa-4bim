b097376ead76f4c7a8d6c6060ef29ce326826abc
Kauã Gondim <79920005+stwdevv@users.noreply.github.com>
2021-11-17 09:39:52 -0300

    Add files via upload

---
+++ javascript/exercicio1.js
@@ -0,0 +1,5 @@
+var texto =" exercicio 1 (conta basica).  " 
+var num1 = 20
+var num2 = 40
+soma = num1 + num2 
+var texto2 =" a soma e igual a "
---
+++ javascript/exercicio2.js
@@ -0,0 +1,7 @@
+function pedirvalores(){
+
+num1 = parseInt(prompt("digite o primeiro numero"));
+num2 = parseInt(prompt("digite o segundo numero"));
+
+soma = num1+num2
+subtracao = num1-num2 }
